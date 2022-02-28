b511eb2bcc8f534f1ebb9721d260096675ad0528
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-28 21:31:03 +0600

    Added base to js

--- script.js
+++ script.js
@@ -79,42 +79,77 @@
 //   container.append(person);
 // });
 
-let company = [
-  {
-    name: "Peter",
-    resume:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, expedita!",
-    profession: "front-end",
-  },
-  {
-    name: "Lasley",
+// let company = [
+//   {
+//     name: "Peter",
+//     resume:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, expedita!",
+//     profession: "front-end",
+//   },
+//   {
+//     name: "Lasley",
 
-    resume:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, expedita!",
-    profession: "java",
+//     resume:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, expedita!",
+//     profession: "java",
+//   },
+//   {
+//     name: "Sam",
+
+//     resume:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, expedita!",
+//     profession: "3D model",
+//   },
+// ]
+// // не правильно 
+// let container = document.querySelector("#container");
+// company.forEach((item) => {
+//   let person = document.createElement("div");
+//   let name = document.createElement("h1");
+//   let resume = document.createElement("p");
+//   let profession = document.createElement("span");
+
+//   h1.textContent = item.name;
+//   p.textContent = item.resume;
+//   span.textContent = item.profession;
+
+//   person.append(name);
+//   person.append(resume);
+//   person.append(profession);
+//   container.append(person);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+let base = [
+  {
+    name: "Ivan",
+    age: 22,
+    img: "https://st2.depositphotos.com/4431055/7495/i/600/depositphotos_74950191-stock-photo-men-latin-american-and-hispanic.jpg",
+    profession: "It-specialist",
+    resume: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt est, alias laboriosam voluptatem neque fugit quasi quos sit obcaecati magnam! Dolor dolorem consequuntur quas cupiditate eos voluptates quae nemo esse!"
   },
   {
-    name: "Sam",
-
-    resume:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, expedita!",
-    profession: "3D model",
+    name: "Clara",
+    age: 19,
+    img: "https://bigpicture.ru/wp-content/uploads/2018/12/100faces00.jpg",
+    profession: "front-end specialist",
+    resume: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt est, alias laboriosam voluptatem neque fugit quasi quos sit obcaecati magnam! Dolor dolorem consequuntur quas cupiditate eos voluptates quae nemo esse!"
   },
+  {
+    name: "Idai",
+    age: 24,
+    img: "https://www.komarov.design/content/images/2020/11/image-4.jpeg",
+    profession: "It-specialist",
+    resume: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt est, alias laboriosam voluptatem neque fugit quasi quos sit obcaecati magnam! Dolor dolorem consequuntur quas cupiditate eos voluptates quae nemo esse!"
+  }
 ]
-// не правильно 
-let container = document.querySelector("#container");
-company.forEach((item) => {
-  let person = document.createElement("div");
-  let name = document.createElement("h1");
-  let resume = document.createElement("p");
-  let profession = document.createElement("span");
-
-  h1.textContent = item.name;
-  p.textContent = item.resume;
-  span.textContent = item.profession;
-
-  person.append(name);
-  person.append(resume);
-  person.append(profession);
-  container.append(person);
-});
